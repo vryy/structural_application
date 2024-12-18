766c4c4e826be3266f099ff63caf1a1207af4263
Hoang-Giang Bui <hgbk2008@gmail.com>
2024-12-18 12:39:12 +0100

    Refactor eigen computation module

--- custom_utilities/eig/eig3.cpp
+++ custom_utilities/eig/eig3.cpp
@@ -21,6 +21,85 @@ void eigen_decomposition(double A[3][3], double V[3][3], double d[3]) {
   eig::tql2<3>(V, d, e);
 }
 
+void spectral_decomposition(
+        double sigma_xx,
+        double sigma_yy,
+        double sigma_zz,
+        double sigma_xy,
+        double sigma_yz,
+        double sigma_zx,
+        double& sigma_1, double& sigma_2, double& sigma_3,
+        array_1d<double, 3>& dir1, array_1d<double, 3>& dir2, array_1d<double, 3>& dir3
+    )
+{
+    double A[3][3];
+    double V[3][3];
+    double d[3];
+
+    A[0][0] = sigma_xx;
+    A[0][1] = sigma_xy;
+    A[0][2] = sigma_zx;
+
+    A[1][0] = sigma_xy;
+    A[1][1] = sigma_yy;
+    A[1][2] = sigma_yz;
+
+    A[2][0] = sigma_zx;
+    A[2][1] = sigma_yz;
+    A[2][2] = sigma_zz;
+
+    eigen_decomposition(A, V, d);
+
+    sigma_1 = d[2];
+    sigma_2 = d[1];
+    sigma_3 = d[0];
+
+    dir1[0] = V[0][2];
+    dir1[1] = V[1][2];
+    dir1[2] = V[2][2];
+
+    dir2[0] = V[0][1];
+    dir2[1] = V[1][1];
+    dir2[2] = V[2][1];
+
+    dir3[0] = V[0][0];
+    dir3[1] = V[1][0];
+    dir3[2] = V[2][0];
+}
+
+void spectral_decomposition(
+        double sigma_xx,
+        double sigma_yy,
+        double sigma_zz,
+        double sigma_xy,
+        double sigma_yz,
+        double sigma_zx,
+        double& sigma_1, double& sigma_2, double& sigma_3
+    )
+{
+    double A[3][3];
+    double V[3][3];
+    double d[3];
+
+    A[0][0] = sigma_xx;
+    A[0][1] = sigma_xy;
+    A[0][2] = sigma_zx;
+
+    A[1][0] = sigma_xy;
+    A[1][1] = sigma_yy;
+    A[1][2] = sigma_yz;
+
+    A[2][0] = sigma_zx;
+    A[2][1] = sigma_yz;
+    A[2][2] = sigma_zz;
+
+    eigen_decomposition(A, V, d);
+
+    sigma_1 = d[2];
+    sigma_2 = d[1];
+    sigma_3 = d[0];
+}
+
 } // end namespace eig3
 
 } // end namespace Kratos
--- custom_utilities/eig/eig3.h
+++ custom_utilities/eig/eig3.h
@@ -5,8 +5,10 @@
 #ifndef _eig3_h
 #define _eig3_h
 
-#include <cmath>
 #include "eig.h"
+
+#include "includes/define.h"
+#include "containers/array_1d.h"
 
 namespace Kratos
 {
@@ -17,6 +19,30 @@ namespace eig3
 /* Symmetric matrix A => eigenvectors in columns of V, corresponding
    eigenvalues in d. */
 void KRATOS_API(STRUCTURAL_APPLICATION) eigen_decomposition(double A[3][3], double V[3][3], double d[3]);
+
+/*
+ * Compute principal stresses and direction using eig3
+ * http://barnesc.blogspot.de/2007/02/eigenvectors-of-3x3-symmetric-matrix.html
+ * Remarks: sigma_1, sigma_2, sigma_3 is sorted
+ */
+void KRATOS_API(STRUCTURAL_APPLICATION) spectral_decomposition(
+                           double sigma_xx,
+                           double sigma_yy,
+                           double sigma_zz,
+                           double sigma_xy,
+                           double sigma_yz,
+                           double sigma_zx,
+                           double& sigma_1, double& sigma_2, double& sigma_3,
+                           array_1d<double, 3>& dir1, array_1d<double, 3>& dir2, array_1d<double, 3>& dir3);
+
+void KRATOS_API(STRUCTURAL_APPLICATION) spectral_decomposition(
+                           double sigma_xx,
+                           double sigma_yy,
+                           double sigma_zz,
+                           double sigma_xy,
+                           double sigma_yz,
+                           double sigma_zx,
+                           double& sigma_1, double& sigma_2, double& sigma_3);
 
 } // end namespace eig3
 
