c2829ee38688c2286f05b02bbadddd61e76afc26
Hoang-Giang Bui <hgbk2008@gmail.com>
2025-04-10 14:44:46 +0100

    Add steady state dynamics analysis to python script

--- python_scripts/structural_solver_advanced.py
+++ python_scripts/structural_solver_advanced.py
@@ -252,6 +252,12 @@ class SolverAdvanced(structural_solver_static.StaticStructuralSolver):
                     raise Exception("time_scheme must be given when analysis_type is -1")
                 self.time_scheme = self.analysis_parameters["time_scheme"]
                 self.MoveMeshFlag = self.analysis_parameters["move_mesh"]
+            elif( self.analysis_parameters['analysis_type'] == 10 ):
+                print("performing frequency analysis")
+                if 'omega' not in self.analysis_parameters:
+                    raise Exception("Omega is necessary for steady state dynamics analysis at specific frequency")
+                omega = self.analysis_parameters['omega']
+                self.time_scheme = SteadyStateDynamicsScheme(omega)
             else:
                 print("analysis type is not defined! Define in analysis_parameters['analysis_type']:")
                 print("   'using static scheme': static analysis")
